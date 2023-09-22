eb0eac508bcf4afa257ec07436651728190a68cb
AlexParreira <alexander.o.parreira@gmail.com>
2023-09-22 14:02:44 -0300

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Alexander Parreira on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
