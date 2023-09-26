f691945ab7055c92013706040a5ada0124c55921
AlexParreira <alexander.o.parreira@gmail.com>
2023-09-26 16:04:39 -0300

    Saving images to the File Manager

--- CryptoApp/Extensions/Double.swift
+++ CryptoApp/Extensions/Double.swift
@@ -45,11 +45,11 @@ extension Double {
         let formatter = NumberFormatter()
         formatter.usesGroupingSeparator = true
         formatter.numberStyle = .currency
-        /*
-        formatter.locale = .current
-        formatter.currencyCode = "usd"
-        formatter.currencySymbol = "$"
-        */
+        
+     //   formatter.locale = .current
+    //    formatter.currencyCode = "usd"
+     //   formatter.currencySymbol = "$"
+        
         formatter.minimumFractionDigits = 2
         formatter.maximumFractionDigits = 6
         return formatter
