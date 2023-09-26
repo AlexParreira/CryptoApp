4453127d2e27b495caedb45389e7f95d66523c10
AlexParreira <alexander.o.parreira@gmail.com>
2023-09-26 15:12:20 -0300

    Downloading Coin Images using Combine, MVVM, and a Networking Layer

---
+++ CryptoApp/Services/CoinImageService.swift
@@ -0,0 +1,34 @@
+//
+//  CoinImageService.swift
+//  CryptoApp
+//
+//  Created by Alexander Parreira on 26/09/23.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    
+    private var imageSubscription: AnyCancellable?
+    private let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        getCoinImage()
+    }
+    func getCoinImage(){
+        guard let url = URL(string: coin.image ) else {return}
+        
+        imageSubscription = NetworkingManager.download(url: url)
+            .tryMap({ (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+                self?.imageSubscription?.cancel()
+            })    }
+}
