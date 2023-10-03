8eee3e50fdf436ffabbbe6f3e7734d7831d647e0
AlexParreira <alexander.o.parreira@gmail.om>
2023-10-02 21:36:26 -0300

    Create a view to manager current user's portfolio

---
+++ CryptoApp/Core/Components/CoinImage/CoinLogoView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinLogoView.swift
+//  CryptoApp
+//
+//  Created by Alexander on 02/10/23.
+//
+
+import SwiftUI
+
+struct CoinLogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinLogoView()
+}
