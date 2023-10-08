de90caf0c3054eb019fa2fb8c76ade9c3061994b
AlexParreira <alexander.o.parreira@gmail.com>
2023-10-08 14:44:39 -0300

    Download coin details by creating a data model, view model, and API request

---
+++ CryptoApp/Core/Detail/ViewModel/DetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewModel.swift
+//  CryptoApp
+//
+//  Created by Alexander on 08/10/23.
+//
+
+import Foundation
---
+++ CryptoApp/Services/CoinDetailDataService.swift
@@ -0,0 +1,8 @@
+//
+//  CoinDetailDataService.swift
+//  CryptoApp
+//
+//  Created by Alexander on 08/10/23.
+//
+
+import Foundation
